477a9e0bd977d3bce4f7c8512177ba7d0fe4db43
Jaya Wikrama <jayawikrama89@gmail.com>
2025-12-22 20:24:10 +0700

    prototype class for duration / elapsed process monitoring.

---
+++ include/duration.hpp
@@ -0,0 +1,40 @@
+#ifndef __DURATION_HPP__
+#define __DURATION_HPP__
+
+#include <chrono>
+#include <vector>
+#include <string>
+
+class Duration
+{
+private:
+    class PointRefs
+    {
+    private:
+        std::chrono::steady_clock::time_point timePoint;
+        std::string caption;
+
+    public:
+        PointRefs(const std::string &caption = "");
+        ~PointRefs();
+
+        const std::chrono::steady_clock::time_point &getTime() const;
+        const std::string &getCaption() const;
+
+        double diff(const std::chrono::steady_clock::time_point &ref) const;
+    };
+
+    std::chrono::steady_clock::time_point startRef;
+    std::vector<PointRefs> pointRefs;
+    std::string caption;
+
+    void printDiffTime(const PointRefs &pref, const std::chrono::steady_clock::time_point &sref) const;
+
+public:
+    Duration(const std::string &caption = "");
+    ~Duration();
+
+    double checkPoint(const std::string &caption = "");
+};
+
+#endif
